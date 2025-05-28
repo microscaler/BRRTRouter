a5d9d623b0af31635c90dfe7e4739e5672e2ce40
casibbald <casibbald@gmail.com>
2025-05-28 21:34:52 +0300

    Adding generated handlers, for testing

--- examples/pet_store/src/handlers/types.rs
+++ examples/pet_store/src/handlers/types.rs
@@ -3,20 +3,9 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize, Default)]
-pub struct Post {
-    pub body: String,
+pub struct GetUserResponse {
     pub id: String,
-    pub title: String,
-    }
-
-#[derive(Debug, Serialize, Deserialize, Default)]
-pub struct GetPetResponse {
-    pub age: i32,
-    pub breed: String,
-    pub id: i32,
     pub name: String,
-    pub tags: Vec<serde_json::Value>,
-    pub vaccinated: bool,
     }
 
 #[derive(Debug, Serialize, Deserialize, Default)]
@@ -27,29 +16,23 @@ pub struct GetPostResponse {
     }
 
 #[derive(Debug, Serialize, Deserialize, Default)]
-pub struct AddPetResponse {
+pub struct PetCreationResponse {
     pub id: i32,
     pub status: String,
     }
 
 #[derive(Debug, Serialize, Deserialize, Default)]
-pub struct GetUserResponse {
-    pub id: String,
-    pub name: String,
-    }
-
-#[derive(Debug, Serialize, Deserialize, Default)]
-pub struct UserList {
+pub struct ListUsersResponse {
     pub users: Vec<User>,
     }
 
 #[derive(Debug, Serialize, Deserialize, Default)]
-pub struct PostItemRequest {
+pub struct CreateItemRequest {
     pub name: String,
     }
 
 #[derive(Debug, Serialize, Deserialize, Default)]
-pub struct PetCreationResponse {
+pub struct AddPetResponse {
     pub id: i32,
     pub status: String,
     }
@@ -61,23 +44,7 @@ pub struct Item {
     }
 
 #[derive(Debug, Serialize, Deserialize, Default)]
-pub struct ListUserPostsResponse {
-    pub items: Vec<Post>,
-    }
-
-#[derive(Debug, Serialize, Deserialize, Default)]
-pub struct AdminSettingsResponse {
-    pub feature_flags: serde_json::Value,
-    }
-
-#[derive(Debug, Serialize, Deserialize, Default)]
-pub struct GetItemResponse {
-    pub id: String,
-    pub name: String,
-    }
-
-#[derive(Debug, Serialize, Deserialize, Default)]
-pub struct CreateItemRequest {
+pub struct PostItemRequest {
     pub name: String,
     }
 
@@ -87,23 +54,8 @@ pub struct Pet {
     pub breed: String,
     pub id: i32,
     pub name: String,
-    pub tags: Vec<serde_json::Value>,
+    pub tags: Vec<String>,
     pub vaccinated: bool,
-    }
-
-#[derive(Debug, Serialize, Deserialize, Default)]
-pub struct AddPetRequest {
-    pub name: String,
-    }
-
-#[derive(Debug, Serialize, Deserialize, Default)]
-pub struct AdminSettings {
-    pub feature_flags: serde_json::Value,
-    }
-
-#[derive(Debug, Serialize, Deserialize, Default)]
-pub struct ListUsersResponse {
-    pub users: Vec<User>,
     }
 
 #[derive(Debug, Serialize, Deserialize, Default)]
@@ -112,18 +64,66 @@ pub struct CreatePetRequest {
     }
 
 #[derive(Debug, Serialize, Deserialize, Default)]
-pub struct ListPetsResponse {
-    pub items: Vec<Pet>,
+pub struct GetPetResponse {
+    pub age: i32,
+    pub breed: String,
+    pub id: i32,
+    pub name: String,
+    pub tags: Vec<String>,
+    pub vaccinated: bool,
     }
 
 #[derive(Debug, Serialize, Deserialize, Default)]
-pub struct PostItemResponse {
+pub struct UserList {
+    pub users: Vec<User>,
+    }
+
+#[derive(Debug, Serialize, Deserialize, Default)]
+pub struct AdminSettingsResponse {
+    pub feature_flags: serde_json::Value,
+    }
+
+#[derive(Debug, Serialize, Deserialize, Default)]
+pub struct User {
     pub id: String,
     pub name: String,
     }
 
 #[derive(Debug, Serialize, Deserialize, Default)]
-pub struct User {
+pub struct GetItemResponse {
+    pub id: String,
+    pub name: String,
+    }
+
+#[derive(Debug, Serialize, Deserialize, Default)]
+pub struct ListUserPostsResponse {
+    pub items: Vec<Post>,
+    }
+
+#[derive(Debug, Serialize, Deserialize, Default)]
+pub struct Post {
+    pub body: String,
+    pub id: String,
+    pub title: String,
+    }
+
+#[derive(Debug, Serialize, Deserialize, Default)]
+pub struct AdminSettings {
+    pub feature_flags: serde_json::Value,
+    }
+
+#[derive(Debug, Serialize, Deserialize, Default)]
+pub struct ListPetsResponse {
+    pub items: Vec<Pet>,
+    }
+
+#[derive(Debug, Serialize, Deserialize, Default)]
+pub struct AddPetRequest {
+    pub name: String,
+    }
+
+#[derive(Debug, Serialize, Deserialize, Default)]
+pub struct PostItemResponse {
     pub id: String,
     pub name: String,
     }
