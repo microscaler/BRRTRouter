3611dd9ca69f179b445367e934066cf0ac719fca
Charles Sibbald <123247+casibbald@users.noreply.github.com>
2025-05-29 19:23:33 +0300

    Regenerate pet_store example

--- examples/pet_store/src/controllers/list_user_posts.rs
+++ examples/pet_store/src/controllers/list_user_posts.rs
@@ -1,7 +1,9 @@
+
 // User-owned controller for handler 'list_user_posts'.
 use crate::brrtrouter::typed::{Handler, TypedHandlerRequest};
-use crate::handlers::list_user_posts::{Request, Response};
+use crate::handlers::list_user_posts::{ Request, Response };
 use crate::handlers::types::Post;
+
 
 pub struct ListUserPostsController;
 
@@ -24,6 +26,7 @@ impl Handler for ListUserPostsController {
         // ]
         Response {
             items: vec![Default::default()],
+            
         }
     }
 }
