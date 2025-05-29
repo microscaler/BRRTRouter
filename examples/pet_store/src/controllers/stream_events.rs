5b82a8cbf119d0a5965c8ec97b02a0d3a4b7b351
Charles Sibbald <123247+casibbald@users.noreply.github.com>
2025-05-29 22:50:33 +0300

    Remove unsafe block in stream_events

--- examples/pet_store/src/controllers/stream_events.rs
+++ examples/pet_store/src/controllers/stream_events.rs
@@ -1,17 +1,16 @@
 use crate::brrtrouter::sse;
 use crate::brrtrouter::dispatcher::{HandlerRequest, HandlerResponse};
-use may::coroutine;
 use std::time::Duration;
 
 pub fn handle(req: HandlerRequest) {
     let (tx, rx) = sse::channel();
     // spawn a coroutine to emit periodic events
-    unsafe { coroutine::spawn(move || {
+    may::go!(move || {
         for i in 0..3 {
             tx.send(format!("tick {i}"));
             may::coroutine::sleep(Duration::from_millis(50));
         }
-    }); }
+    });
     let body = rx.collect();
     let resp = HandlerResponse { status: 200, body: serde_json::Value::String(body) };
     let _ = req.reply_tx.send(resp);
