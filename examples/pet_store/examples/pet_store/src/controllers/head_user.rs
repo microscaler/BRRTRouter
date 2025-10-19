36a0fc14f45d68900772a1256679103dc92933e6
casibbald <casibbald@gmail.com>
2025-10-20 01:11:04 +0300

    Memory stats

---
+++ examples/pet_store/examples/pet_store/src/controllers/head_user.rs
@@ -0,0 +1,23 @@
+
+// User-owned controller for handler 'head_user'.
+use brrtrouter_macros::handler;
+use crate::brrtrouter::typed::TypedHandlerRequest;
+use crate::handlers::head_user::{ Request, Response };
+
+
+
+#[handler(HeadUserController)]
+pub fn handle(_req: TypedHandlerRequest<Request>) -> Response {
+    
+    // Example response:
+        // {
+        //   "exists": true
+        // }
+    
+    Response {
+        exists: Some(true),
+        
+    }
+    
+    
+}
