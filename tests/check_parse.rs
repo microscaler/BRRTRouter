960f8d7eb1e09a581174e84f813ba8a8a36fa6a2
Charles Sibbald <casibbald@gmail.com>
2026-03-27 08:20:56 +0200

    docs(readme): announce non-blocking BFF Auto-Proxy integration support

--- tests/check_parse.rs
+++
@@ -1,7 +0,0 @@
-#[test]
-fn test_parse_bff() {
-    let spec = oas3::from_path("/Users/casibbald/Workspace/hauliage/openapi/openapi_bff.yaml").unwrap();
-    let op = spec.paths.as_ref().unwrap().get("/api/v1/consignments/ai/tidy-text").unwrap().post.as_ref().unwrap();
-    println!("Extensions: {:?}", op.extensions);
-    assert!(op.extensions.contains_key("x-service"), "x-service is missing in oas3 parser output!");
-}
--- tests/dump_keys.rs
+++
@@ -1,6 +0,0 @@
-#[test]
-fn test_dump() {
-    let spec = oas3::from_path("/Users/casibbald/Workspace/hauliage/openapi/openapi_bff.yaml").unwrap();
-    let op = spec.paths.as_ref().unwrap().get("/api/v1/consignments/ai/tidy-text").unwrap().post.as_ref().unwrap();
-    println!("EXTENSIONS DICT: {:?}", op.extensions.keys().collect::<Vec<_>>());
-}
